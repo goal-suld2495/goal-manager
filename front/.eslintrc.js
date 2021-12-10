9b411f3348b4fc01855c7554195dd44c8b056258
suld2495 <suld7777@gmail.com>
2021-12-11 00:42:50 +0900

    feat : 환경설정 추가
    eslint 및 prettier에 관련 된 환경설정을 추가하였습니다.

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -3,24 +3,42 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['prettier', 'jsx-a11y', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-empty-function': ['error', { allow: ['methods', 'arrowFunctions'] }],
+    '@typescript-eslint/no-empty-function': [
+      'error',
+      { allow: ['methods', 'arrowFunctions'] },
+    ],
     '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-explicit-any': ['error'],
     'no-console': ['error'],
     'no-debugger': ['error'],
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react/jsx-filename-extension': 0,
+    'linebreak-style': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
-
-// TODO
-// 맛달 프로젝트 보면서 환경 설정 셋팅
-// 기본적인 index.ts 파일 구성
-// 깃허브 프로젝트 스프린트 구성
