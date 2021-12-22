d02a3dc0cd77a832ba368d415fcd058f43a63f1e
suld2495 <suld7777@gmail.com>
2021-12-22 21:02:37 +0900

    feat : 사용자가 입력한 값을 상태로 저장 후 생성 버튼 클릭 시 함께 전달

--- front/src/components/memo/MemoForm.tsx
+++ front/src/components/memo/MemoForm.tsx
@@ -1,24 +1,60 @@
-import React, { EventHandler } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const MemoFormBlock = styled.form``;
 
+interface Memo {
+  title: string;
+  content: string;
+}
+
 type MemoFormProps = {
-  onSubmit: EventHandler<React.MouseEvent>;
+  onSubmit: (form: Memo) => void;
 };
 
 const MemoForm = ({ onSubmit }: MemoFormProps) => {
+  const [form, setForm] = useState({
+    title: '',
+    content: '',
+  });
+
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const onClick = () => {
+    if (!form.title || !form.content) {
+      return;
+    }
+
+    onSubmit(form);
+  };
+
   return (
     <MemoFormBlock>
       <div className="title">
         <label htmlFor="title">제목</label>
-        <input id="title" type="text" />
+        <input
+          id="title"
+          name="title"
+          type="text"
+          value={form.title}
+          onChange={onChange}
+        />
       </div>
       <div className="content">
-        <textarea placeholder="내용을 입력해 주세요." />
+        <textarea
+          name="content"
+          placeholder="내용을 입력해 주세요."
+          value={form.content}
+          onChange={onChange}
+        />
       </div>
       <div className="buttons">
-        <button type="button" onClick={onSubmit}>
+        <button type="button" onClick={onClick}>
           생성
         </button>
       </div>
--- front/src/components/memo/__test__/MemoForm.test.tsx
+++ front/src/components/memo/__test__/MemoForm.test.tsx
@@ -12,13 +12,34 @@ describe('MemoForm', () => {
     expect(screen.getByRole('button', { name: '생성' })).toBeInTheDocument();
   });
 
-  it('생성 버튼을 클릭 시 전송 기능이 호출 된다.', () => {
+  it('타이틀 및 내용을 작성하지 않은 경우 전송 기능이 호출 되지 않는다.', () => {
     const fn = jest.fn();
     render(<MemoForm onSubmit={fn} />);
 
     const button = screen.getByRole('button', { name: '생성' });
     userEvent.click(button);
 
-    expect(fn).toHaveBeenCalled();
+    expect(fn).not.toHaveBeenCalled();
+  });
+
+  it('전송 시 파라미터는 사용자가 입력한 타이틀과 내용입니다.', () => {
+    const fn = jest.fn();
+    render(<MemoForm onSubmit={fn} />);
+
+    const form = {
+      title: '여기는 제목입니다.',
+      content: '여기는 내용입니다.',
+    };
+
+    const titleInput = screen.getByLabelText('제목');
+    userEvent.type(titleInput, form.title);
+
+    const contentInput = screen.getByPlaceholderText('내용을 입력해 주세요.');
+    userEvent.type(contentInput, form.content);
+
+    const button = screen.getByRole('button', { name: '생성' });
+    userEvent.click(button);
+
+    expect(fn).toHaveBeenCalledWith(form);
   });
 });
