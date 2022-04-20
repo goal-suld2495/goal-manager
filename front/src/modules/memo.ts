01dc1771d038a6ec0ca6183f0dcc6e58a02b45db
suld2495 <suld7777@gmail.com>
2022-04-20 20:02:30 +0900

    refactor : 리덕스를 리코일로 변환

--- front/src/modules/memo.ts
+++ front/src/modules/memo.ts
@@ -6,6 +6,7 @@ import {
   createReducer,
 } from 'typesafe-actions';
 import { AxiosResponse } from 'axios';
+import { atom, selector, selectorFamily } from 'recoil';
 import { Form, Memo } from '../types';
 import * as memoApi from '../lib/api/memo';
 import {
@@ -99,3 +100,46 @@ const memo = createReducer<MemoState, MemoAction>(initialState)
   }));
 
 export default memo;
+
+/*
+  -----------------------
+*/
+
+export const formState = atom<Form>({
+  key: 'formState',
+  default: {
+    title: '',
+    content: '',
+  },
+});
+
+export const memoState = atom<Memo>({
+  key: 'memoState',
+  default: {
+    id: '',
+    title: '',
+    content: '',
+  },
+});
+
+export const memoErrorState = atom<Error | null>({
+  key: 'memoErrorState',
+  default: null,
+});
+
+export const memoIdState = atom<string>({
+  key: 'memoIdState',
+  default: '',
+});
+
+export const CurrentMemoState = selectorFamily<Memo, string>({
+  key: 'CurrentMemoState',
+  get: (memoId) => async () => {
+    try {
+      const response = await memoApi.fetchMemo(memoId);
+      return response.data;
+    } catch {
+      throw new Error('');
+    }
+  },
+});
